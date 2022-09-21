4e08443a90ffd20c87a43939f4c02522dd4769e8
Dave Berzack <daveberzack@gmail.com>
2022-09-20 20:48:03 -0400

    a nice message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          CI/CD Version 3
+          RIP The Queen.
         </a>
       </header>
     </div>
